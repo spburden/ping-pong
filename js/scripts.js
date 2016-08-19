dc43d5fb9f36d571e5c6fa2937d9eb86919808f3
Stephen Burden <spburden@hotmail.com>
2016-08-19 08:14:52 -0700

    add user input validation to js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ $(function() {
  $("form").submit(function(event) {
  event.preventDefault();
  var inputNumber = parseInt($("input").val());
+ // if(isNaN(inputNumber)){
+ //   $("#warning").show;
+ // } else {
+ //   pingPong(inputNumber).slidedown;
+ // }
  alert(inputNumber);
   });
 });
