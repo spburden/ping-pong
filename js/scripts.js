40614dcc245da3ef9335054849432dfa4268ed39
Stephen Burden <spburden@hotmail.com>
2016-08-19 08:25:58 -0700

    create a for loop which outputs 'ping' for a number divisible by 3 into an array called outputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 //BUSINESS LOGIC
+function pingPong(number){
+  var outputs = [];
+  for (var i = 1; i <= number; i++){
+    if(i%3 === 0){
+      outputs.push("ping");
 
+    }
+  }
+
+}
 
 //USER INTERFACE LOGIC
-$(function() {
- $("form").submit(function(event) {
+$(function(){
+ $("form").submit(function(event){
  event.preventDefault();
  var inputNumber = parseInt($("input").val());
  // if(isNaN(inputNumber)){
  //   $("#warning").show;
- // } else {
+ // }else{
  //   pingPong(inputNumber).slidedown;
  // }
  alert(inputNumber);
