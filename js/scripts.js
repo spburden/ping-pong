0cae99ab77ee50f32480a0cf2706cd7ba5a6b9d7
Stephen Burden <spburden@hotmail.com>
2016-08-19 08:39:36 -0700

    add conditions for numbers divisible by 15 and 5 in to the loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
 //BUSINESS LOGIC
-function pingPong(number){
+function pingPong(number) {
   var outputs = [];
-  for (var i = 1; i <= number; i++){
-    if(i%3 === 0){
+  for (var i = 1; i <= number; i++) {
+    if(i%15 === 0) {
+      outputs.push("ping-pong");
+    } else if(i%3 === 0) {
       outputs.push("ping");
-
+    } else if(i%5 === 0) {
+      outputs.push("pong");
+    } else {
+      outputs.push(i);
     }
-  }
-
-}
+  };
+  return outputs;
+};
 
 //USER INTERFACE LOGIC
-$(function(){
- $("form").submit(function(event){
+$(function() {
+ $("form").submit(function(event) {
  event.preventDefault();
  var inputNumber = parseInt($("input").val());
- // if(isNaN(inputNumber)){
+ // if(isNaN(inputNumber)) {
  //   $("#warning").show;
- // }else{
+ // } else {
  //   pingPong(inputNumber).slidedown;
  // }
  alert(inputNumber);
+ console.log(pingPong(inputNumber));
   });
 });
